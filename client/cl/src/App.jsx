62b15174ec6572dca44412a86a4c531e9a2cb274
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-02-09 14:29:18 +0530

    Setup talwind cssand shadcn ui

--- client/cl/src/App.jsx
+++ client/cl/src/App.jsx
@@ -1,8 +1,13 @@
+import './App.css'
+import {Button} from './components/ui/button.jsx'
 function App(){
   return (
-    <h1>
-      Welcome Back 
+    <>
+    <h1 className="text-4xl font-bold underline  ">
+      Chaliye Shuru krte hai.
     </h1>
+    <Button className="bg-black text-white hover:bg-black/80">Press it.</Button>
+  </>
   )
 }
 
