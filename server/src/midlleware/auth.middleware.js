c4a0e9ba24049b9ed69f684c337c9af0181e5b06
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-02-10 18:35:44 +0530

    added auth middleware

---
+++ server/src/midlleware/auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+
+
+const authMiddleware = async(req,res,next)=>{
+
+    const authHeaders = req.headers.authorization
+    
+    if(!authHeaders || !authHeaders.startWith("Bearer ")){
+        return res.status(401).json({message:"Not authorized"})
+    }
+
+    const token = authHeaders.split(" ")[1]
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = {id:decoded.id}
+        next();
+    }       
+    catch(err){
+        console.log(err)
+        return res.status(401).json({message:"Invalid Token."})
+    }
+};
+
+export default authMiddleware
