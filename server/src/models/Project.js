001ca597c2858ae69e50b0537d1fa7f30cc06c78
Bhavishya Sharma <bhavishaya32@gmail.com>
2026-02-24 13:06:46 +0530

    fix: remove apiKey index from Project schema

--- server/src/models/Project.js
+++ server/src/models/Project.js
@@ -24,7 +24,7 @@ const projectSchema = new mongoose.Schema({
     {timestamps:true}
 )
 projectSchema.index({ ownerId: 1 });
-projectSchema.index({ apiKey: 1 });
+
 
 
 export default mongoose.model("Project",projectSchema)
